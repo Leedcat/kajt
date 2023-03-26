83647111a3266a7d5476bc372ed0927b30ce64b6
pupu9407 <edvseh@edu.umea.se>
2023-03-26 16:30:49 +0200

    beep boop
    Hoppas allt gick bra

--- Assets/Scripts/TransparentWindow.cs
+++ Assets/Scripts/TransparentWindow.cs
@@ -7,10 +7,16 @@ using UnityEngine;
 public class TransparentWindow : MonoBehaviour
 {
     [DllImport("user32.dll")]
-    private static extern IntPtr GetActiveWindow();
+    private static extern IntPtr SetActiveWindow(IntPtr hWnd);
 
     [DllImport("user32.dll")]
-    private static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
+    public static extern IntPtr FindWindow(string className, string windowName);
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetActiveWindow(); 
+
+    [DllImport("user32.dll")]
+    private static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong); //Set atributes for a window
 
     [DllImport("user32.dll")]
     static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInserAfter, int X, int Y, int cx, int xy, uint uFlags);
@@ -19,7 +25,7 @@ public class TransparentWindow : MonoBehaviour
     { public int cxLeftWidth; public int cxRightWidth; public int xyTopHeight; public int cyBottomHeight; }
 
     [DllImport("Dwmapi.dll")]
-    private static extern uint DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS margins);
+    private static extern uint DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS margins); //Allows to set window margins
 
     const int GWL_EXSTYLE = -20;
 
@@ -28,17 +34,17 @@ public class TransparentWindow : MonoBehaviour
 
     static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
 
-    private void Start()
-    {
-#if !UNITY_EDITOR
-        IntPtr hWnd = GetActiveWindow();
-        MARGINS margins = new MARGINS { cxLeftWidth = -1 };
-        DwmExtendFrameIntoClientArea(hWnd, ref margins);
+    private void Start() {
+#if !UNITY_EDITOR //Only run if outside of unity editor
+        IntPtr hWnd = FindWindow(null,"KiteTrainer"); //Makes unity program the active window
+        MARGINS margins = new MARGINS { cxLeftWidth = -1 }; //Negative value makes the window transparent
+        DwmExtendFrameIntoClientArea(hWnd, ref margins); //Makes the window transparant
 
-        SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TRANSPARENT);
 
-        SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, 0);
+        SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TRANSPARENT); //Makes the window clickthrough
+
+        SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, 0); //Unity always stay on top
 #endif
-        Application.runInBackground = true;
+        Application.runInBackground = true; //Program runs in the background
     }
 }
